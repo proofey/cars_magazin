3e2262024676ee1680c5e179d43e9fbb58401c8b
Mincho Rusev <proofey89@gmail.com>
2022-04-06 12:26:18 +0300

    add active navbar

--- static/base.js
+++ static/base.js
@@ -1,4 +1,14 @@
 $(document).ready(function(){
-    $('#registrationModal').modal('show')
-    $('#loginModal').modal('show')
+    $('#registrationModal').modal('show');
+    $('#loginModal').modal('show');
 });
+
+// active navbar
+var currentLocation = location.href;
+var navLink = document.querySelectorAll('.nav-link');
+var navLenght = navLink.length;
+for(let i=0; i<navLenght; i++){
+    if(navLink[i].href === currentLocation){
+        navLink[i].classList.add('active-tab');
+    }
+}
