9683a26b1fb2f42b945ae6df9c47bc85f1b61940
Mincho Rusev <proofey89@gmail.com>
2022-04-12 20:15:39 +0300

    add Post models

--- post/admin.py
+++ post/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from . models import Post, Coupe, TypeExtra, Extra, Gearbox, Condition, Fuel, NumberOfDoors
 
-# Register your models here.
+
+admin.site.register(Post)
+admin.site.register(Coupe)
+admin.site.register(TypeExtra)
+admin.site.register(Extra)
+admin.site.register(Gearbox)
+admin.site.register(Condition)
+admin.site.register(Fuel)
+admin.site.register(NumberOfDoors)
--- post/models.py
+++ post/models.py
@@ -1,6 +1,76 @@
 from django.db import models
+from user_profile.models import Profile
+
+
+
+
+class TypeExtra(models.Model):
+    type = models.CharField(verbose_name='Type extra', max_length=100)
+
+    def __str__(self):
+        return self.type
+
+class Extra(models.Model):
+    type_extra = models.ForeignKey(TypeExtra, on_delete=models.CASCADE)
+    extra = models.CharField(verbose_name='Extra', max_length=100)
+
+    def __str__(self):
+        return f"{self.type_extra} - {self.extra}"
+
+
+
+class Coupe(models.Model):
+    type = models.CharField(verbose_name='Coupe', max_length=30)
+
+    def __str__(self):
+        return self.type
+
+class Gearbox(models.Model):
+    type = models.CharField(verbose_name='Gearbox', max_length=30)
+
+    def __str__(self):
+        return self.type
+
+class Condition(models.Model):
+    type = models.CharField(verbose_name='Condition', max_length=30)
+
+    def __str__(self):
+        return self.type
+
+class Fuel(models.Model):
+    type = models.CharField(verbose_name='Fuel', max_length=20)
+
+    def __str__(self):
+        return self.type
+
+class NumberOfDoors(models.Model):
+    number_of_doors = models.CharField(verbose_name='Number of doors', max_length=10)
+
+    def __str__(self):
+        return self.number_of_doors
+
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=100)
-    
+    author = models.OneToOneField(Profile, on_delete=models.CASCADE)
+    model = models.CharField(verbose_name='Model', max_length=100)
+    price = models.IntegerField(verbose_name='Price')
+    price_negotiable = models.BooleanField(verbose_name='Price negotiable', blank=True, null=True)
+    year_made = models.IntegerField(verbose_name='Year')
+    horsepower = models.IntegerField(verbose_name='Horsepower', blank=True, null=True)
+    engine_size = models.IntegerField(verbose_name='Engine size', blank=True, null=True)
+    mileage = models.IntegerField(verbose_name='Mileage')
+    color = models.CharField(verbose_name='Color', max_length=30, blank=True, null=True)
+    new_importation = models.BooleanField(verbose_name='New importation', blank=True, null=True)
+    located_at = models.CharField(verbose_name='Located at', max_length=30)
+    more_info = models.TextField(verbose_name='More information', blank=True, null=True)
+
+    extras = models.ManyToManyField(Extra, blank=True)
+    coupe = models.ForeignKey(Coupe, on_delete=models.PROTECT, null=True)
+    gearbox = models.ForeignKey(Gearbox, on_delete=models.PROTECT, null=True)
+    condition = models.ForeignKey(Condition, on_delete=models.PROTECT, null=True)
+    fuel = models.ForeignKey(Fuel, on_delete=models.PROTECT, null=True)
+    number_of_doors = models.ForeignKey(NumberOfDoors, on_delete=models.PROTECT, null=True)
+
+    def __str__(self):
+        return f"{self.author} - {self.model} - {self.price}"
